e6a3fdfccbf824cddba8d3ca8d8dd043791cbb4d
yuan-miranda <yuanezekielamiranda@gmail.com>
2026-06-08 07:46:20 +0800

    Enhance OTP handling in telegram.py with additional comments and adjust response delay

--- telegram.py
+++ telegram.py
@@ -11,12 +11,16 @@ api_hash = os.getenv("API_HASH")
 client = TelegramClient("login", api_id, api_hash)
 
 async def get_otp_code():
+    # trigger /click=name from the esp32 web server
+    # - this would send a query to esp32 to activate the solenoid of said device.
+    # trigger scanning for the OTP code
+    #  - using webcam it would take a picture and pass it to Gemini and extract the OTP (returns json)
     return str(random.randint(100000, 999999))
 
 @client.on(events.NewMessage(chats="otp_22"))
 async def handle_otp_requests(event):
-    if event.out:
-        return
+    # if event.out:
+    #     return
 
     text = event.raw_text.strip().lower()
     reply_text = ""
@@ -38,7 +42,7 @@ async def handle_otp_requests(event):
         reply_text = await get_otp_code()
 
     if reply_text:
-        await asyncio.sleep(random.uniform(1, 3))
+        await asyncio.sleep(random.uniform(3, 6))
         await client.send_message(event.chat_id, reply_text)
 
 
