e794fe6ae71ed4041a8b196419f5cbe2a84a54c9
yuan-miranda <yuanezekielamiranda@gmail.com>
2026-06-24 08:28:19 +0800

    updated model

--- gemini.py
+++ gemini.py
@@ -29,8 +29,8 @@ def get_extracted_otps(image_path):
         ref_image.thumbnail((768, 768))
 
         response = client.models.generate_content(
-            model="gemini-3.5-flash",
-            # model="gemini-3.1-flash-lite",
+            # model="gemini-3.5-flash",
+            model="gemini-3.1-flash-lite",
             contents=[image, ref_image],
             config=types.GenerateContentConfig(
                 system_instruction=(
